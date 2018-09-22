8a85ef38e312a56857fe70817df43e98908ff1b1
Sebastian Siejek <sebastiansiejek@MacBook-Pro-Sebastian.local>
2018-09-22 17:58:46 +0200

    add webpack.config.js with core

---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const isProduction = process.env.NODE_ENV === "production";
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    bundle: ["babel-polyfill", "./src/assets/app"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "[name].[ext]",
+              publicPath: "../images"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: "../fonts"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            { loader: "postcss-loader" }
+          ]
+        })
+      },
+      {
+        test: /\.scss|sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+    new WebpackNotifierPlugin({
+      alwaysNotify: true
+    }),
+    new ExtractTextPlugin({
+      filename: "css/[name].css",
+      disable: false
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: isProduction
+    }),
+  ]
+};
+
+if (isProduction) {
+  module.exports.plugins.push(
+    new CleanWebpackPlugin("dist"),
+  );
+}
