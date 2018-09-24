f4d38ccd3458e932cefcf6ba1088dfe0ad70f042
Sebastian Siejek <siejeksebastian@gmail.com>
2018-09-24 15:16:35 +0200

    initilaized

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const isProduction = process.env.NODE_ENV === "production";
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const WebpackNotifierPlugin = require("webpack-notifier");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: {
-    bundle: ["babel-polyfill", "./src/assets/app"],
+    bundle: ['babel-polyfill', './src/index.js'],
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "js/[name].js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/[name].js',
   },
   module: {
     rules: [
@@ -21,96 +22,107 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              name: "[name].[ext]",
-              publicPath: "../images"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+              publicPath: '../images',
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-              publicPath: "../fonts"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: '../fonts',
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["env"]
-          }
-        }
+            presets: ['env'],
+          },
+        },
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
-            { loader: "postcss-loader" }
-          ]
-        })
+            { loader: 'postcss-loader' },
+          ],
+        }),
       },
       {
         test: /\.scss|sass$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
-              loader: "postcss-loader"
+              loader: 'postcss-loader',
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      }
-    ]
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+    ],
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
     new WebpackNotifierPlugin({
-      alwaysNotify: true
+      alwaysNotify: true,
     }),
     new ExtractTextPlugin({
-      filename: "css/[name].css",
-      disable: false
+      filename: 'css/[name].css',
+      disable: false,
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: isProduction
+      minimize: isProduction,
     }),
-  ]
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      hash: true,
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    stats: 'errors-only',
+    open: true,
+  },
 };
 
 if (isProduction) {
   module.exports.plugins.push(
-    new CleanWebpackPlugin("dist"),
+    new CleanWebpackPlugin('dist'),
   );
 }
